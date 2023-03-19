77f821a9fad4c5f365ce9b7411255292d28e8b5c
sverhaeg <77927059+sverhaeg@users.noreply.github.com>
2023-03-19 23:30:39 +0100

    Update cron_queue_process.sh

--- all_custom/cron_queue_process.sh
+++ all_custom/cron_queue_process.sh
@@ -21,7 +21,7 @@ plexsection="https://${serverport}/library/sections/${section}?X-Plex-Token=${to
     # now=$(date +"%x_%X")
     # Only show time
     now=$(date +"%H%I ")
-    echo -n "=${now}=" >> ${mylogfile}
+    echo -n "${now}=" >> ${mylogfile}
     #echo "==== plex section check=" >> ${mylogfile}
     #curl -k ${plexsection} | grep -e key\=\"all\" >> ${mylogfile}
     cd "${mydir}"
